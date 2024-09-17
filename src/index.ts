3f5e3a117505d84b4479a27968c8e4df2bb4ec41
Diva Nadya Pasha Ramadhani <divanadya1124@gmail.com>
2024-09-17 11:01:51 +0700

    praktikum api

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import Express from "express"
+import MedicineRoute from "./router/medicineRouter"
+
+const app = Express()
+/** allow to read a body request with JSON format */
+app.use(Express.json())
+
+/** prefix for medicine root */
+app.use(`/medicine`, MedicineRoute)
+
+const PORT = 1229
+app.listen(PORT, () => { 
+    console.log(`Server Drugstore run on  port ${PORT}`)
+})
